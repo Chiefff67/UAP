815283ec0628815761ee01eb2aa1577c57c24c90
Xkylz1 <fauzansaputra741@gmail.com>
2022-11-23 15:09:12 +0800

    daftar login

--- UAP.java
+++ UAP.java
@@ -1,26 +1,37 @@
 package org.example;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class UAP {
     public static void main(String[] args) {
         Scanner inputMenu = new Scanner(System.in);
+        Scanner inputStr = new Scanner(System.in);
         //var log cred
         boolean terdaftar = false;
-        String nama;
-        String username;
-        String NIM;
+        String nama=null;
+        String NIM=null;
+        String username=null;
 
         //menu pertama
         int menu;
         do {
             menuAdministrasi();
             menu = inputMenu.nextInt();
-            switch (menu){
+            switch (menu) {
                 case 1:
-                    
+                    System.out.print("nama");
+                    nama = inputStr.nextLine();
+                    System.out.print("NIM");
+                    NIM = inputStr.nextLine();
+                    username = namaAkhir(nama);
                     break;
                 case 2:
+                    System.out.print("user");
+                    String loginUsername=inputStr.nextLine();
+                    System.out.print("pass");
+                    String loginPass=inputStr.nextLine();
+                    terdaftar=login(loginUsername,loginPass,username,NIM);
                     break;
                 case 3:
                     System.out.println("Terima kasih!");
@@ -45,6 +56,22 @@ public class Main {
         System.out.println("Pilih menu : ");
     }
 
+    public static String namaAkhir(String namaLengkap) {
+        String namapembagi[] = namaLengkap.split(" ");
+        String username = namapembagi[namapembagi.length - 1].toLowerCase(Locale.ROOT);
+        return username;
+    }
+
+    public static boolean login(String user,String pass,String dataUser, String PassUser){
+        boolean terdaftar=false;
+        if(user.equals(dataUser)&&pass.equals(PassUser)){
+            terdaftar=true;
+        }else {
+            Error();
+        }
+        return terdaftar;
+    }
+
 //    public static void daftar(){
 //        nama = anjay;
 //    }
@@ -65,7 +92,8 @@ public class Main {
         for (int i = 0; i < 100; i++)
             System.out.println();
     }
-    public static void Error(){
+
+    public static void Error() {
         System.out.println("Input salah!");
     }
 }
---
+++ pseuducode.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class pseuducode {
+    public static void main(String[] args) {
+        String nama = "Anjay mabar";
+        String nama1[]=nama.split(" ");
+        System.out.println(nama1[1]);
+    }
+}
