2d3a24b98eb46fd8d53fee415aed34c4bb983177
Xkylz1 <fauzansaputra741@gmail.com>
2022-11-24 21:35:26 +0800

    menuju laprak

--- UAP.java
+++ UAP.java
@@ -43,6 +43,8 @@ public class UAP {
                             System.out.println("Login sukses!");
 //                            garis();
 //                            menuPerpus();
+                        } else {
+                            Error();
                         }
                     } else {
                         garis();
@@ -53,17 +55,43 @@ public class UAP {
                     System.out.println("Terima kasih!");
                     break;
                 default:
+                    Error();
 
             }
-        } while (menu != 3 && !loginStatus);
+        } while (menu != 3);
 
 
         //daftar dan login
 
         //Daftar buku
 //index = id
-        String judulBuku = "Bumi";
-        int jumlahBuku = 5;
+        String judulBuku1 = "Bumi";
+        int jumlahBuku1 = 9;
+        String judulBuku2 = "Bulan";
+        int jumlahBuku2 = 8;
+        String judulBuku3 = "Matahari";
+        int jumlahBuku3 = 7;
+        String judulBuku4 = "Bintang";
+        int jumlahBuku4 = 6;
+        String judulBuku5 = "Ceroz dan Batozar";
+        int jumlahBuku5 = 5;
+        String judulBuku6 = "Komet";
+        int jumlahBuku6 = 5;
+        String judulBuku7 = "Komet Minor";
+        int jumlahBuku7 = 4;
+        String judulBuku8 = "Selena";
+        int jumlahBuku8 = 3;
+        String judulBuku9 = "Nebula";
+        int jumlahBuku9 = 3;
+        String judulBuku10 = "S Putih";
+        int jumlahBuku10 = 2;
+        String judulBuku11 = "Lumpu";
+        int jumlahBuku11 = 2;
+        String judulBuku12 = "Bibi Gili";
+        int jumlahBuku12 = 1;
+        String judulBuku13 = "Sagara";
+        int jumlahBuku13 = 1;
+
         //menu selanjutnya
         int menuPerpus;
         do {
@@ -71,10 +99,99 @@ public class UAP {
             System.out.println("1. Tampilkan daftar buku\n2. Pinjam Buku\n3. Request Buku\n4. Keluar");
             System.out.print("Pilih menu : ");
             menuPerpus = inputMenu.nextInt();
+            switch (menuPerpus) {
+                case 1:
+                    daftarBuku();
+                    break;
+                case 2:
+                    garis();
+                    daftarBuku();
+                    garis();
+                    System.out.println("Pilih buku: ");
+                    int indexBuku = inputMenu.nextInt();
+                    switch (indexBuku){
+                        case 1:
+                            jumlahBuku1=pinjamBuku(jumlahBuku1);
+                            System.out.println("anda meminjam "+judulBuku1);
+                            System.out.println(judulBuku1+" yang tersedia sekarang sebanyak "+jumlahBuku1+" buku");
+                            break;
+
+                        case 2:
+                            jumlahBuku2=pinjamBuku(jumlahBuku2);
+                            System.out.println("anda meminjam "+judulBuku2);
+                            System.out.println(judulBuku2+" yang tersedia sekarang sebanyak "+jumlahBuku2+" buku");
+                            break;
+                        case 3:
+                            jumlahBuku3=pinjamBuku(jumlahBuku3);
+                            System.out.println("anda meminjam "+judulBuku3);
+                            System.out.println(judulBuku3+" yang tersedia sekarang sebanyak "+jumlahBuku3+" buku");
+                            break;
+                        case 4:
+                            jumlahBuku4=pinjamBuku(jumlahBuku4);
+                            System.out.println("anda meminjam "+judulBuku4);
+                            System.out.println(judulBuku4+" yang tersedia sekarang sebanyak "+jumlahBuku4+" buku");
+                            break;
+                        case 5:
+                            jumlahBuku5=pinjamBuku(jumlahBuku5);
+                            System.out.println("anda meminjam "+judulBuku5);
+                            System.out.println(judulBuku5+" yang tersedia sekarang sebanyak "+jumlahBuku5+" buku");
+                            break;
+                        case 6:
+                            jumlahBuku6=pinjamBuku(jumlahBuku6);
+                            System.out.println("anda meminjam "+judulBuku6);
+                            System.out.println(judulBuku6+" yang tersedia sekarang sebanyak "+jumlahBuku6+" buku");
+                            break;
+                        case 7:
+                            jumlahBuku7=pinjamBuku(jumlahBuku7);
+                            System.out.println("anda meminjam "+judulBuku7);
+                            System.out.println(judulBuku7+" yang tersedia sekarang sebanyak "+jumlahBuku7+" buku");
+                            break;
+
+                        case 8:
+                            jumlahBuku8=pinjamBuku(jumlahBuku8);
+                            System.out.println("anda meminjam "+judulBuku8);
+                            System.out.println(judulBuku8+" yang tersedia sekarang sebanyak "+jumlahBuku8+" buku");
+                            break;
+                        case 9:
+                            jumlahBuku9=pinjamBuku(jumlahBuku9);
+                            System.out.println("anda meminjam "+judulBuku9);
+                            System.out.println(judulBuku9+" yang tersedia sekarang sebanyak "+jumlahBuku9+" buku");
+                            break;
+                        case 10:
+                            jumlahBuku1=pinjamBuku(jumlahBuku10);
+                            System.out.println("anda meminjam "+judulBuku10);
+                            System.out.println(judulBuku10+" yang tersedia sekarang sebanyak "+jumlahBuku10+" buku");
+                            break;
+                        case 11:
+                            jumlahBuku11=pinjamBuku(jumlahBuku1);
+                            System.out.println("anda meminjam "+judulBuku11);
+                            System.out.println(judulBuku11+" yang tersedia sekarang sebanyak "+jumlahBuku1+" buku");
+                            break;
+                        case 12:
+                            jumlahBuku12=pinjamBuku(jumlahBuku12);
+                            System.out.println("anda meminjam "+judulBuku12);
+                            System.out.println(judulBuku12+" yang tersedia sekarang sebanyak "+jumlahBuku12+" buku");
+                            break;
+                        case 13:
+                            jumlahBuku13=pinjamBuku(jumlahBuku13);
+                            System.out.println("anda meminjam "+judulBuku13);
+                            System.out.println(judulBuku13+" yang tersedia sekarang sebanyak "+jumlahBuku13+" buku");
+                            break;
+                    }
+                    break;
+                case 3:
+                    System.out.print("Judul buku yang di request: ");
+                    String capeGaBolehPakeArray=inputStr.nextLine();
+                    requestBuku(capeGaBolehPakeArray);
+                    break;
+                case 4:
+                    System.out.println("Terima kasih!");
+                default:
+                    Error();
+            }
+
+
         } while (menuPerpus != 4);
-        daftarBuku();
-        pinjamBuku();
-        requestBuku();
     }
 
     public static void menuAdministrasi() {
@@ -107,21 +224,21 @@ public class UAP {
     }
 
     public static void daftarBuku() {
-
+        System.out.println("1. Bumi\n2. Bulan\n3. Matahari\n4. Bintang\n5. Ceros dan Batozar\n6. Komet \n7. Komet Minor\n8. Selena\n9. Nebula\n10. Si Putih\n11. Lumpu\n12. Bibi Gili\n13. Sagaras");
     }
 
-    public static void pinjamBuku() {
-
+    public static int pinjamBuku(int jumlahSekarang) {
+        return jumlahSekarang - 1;
     }
 
-    public static void requestBuku() {
-
+    public static void requestBuku(String judul) {
+        System.out.println(judul+" akan tersedia bulan Depan!");
     }
 
-    public static void clear() {
-        for (int i = 0; i < 100; i++)
-            System.out.println();
-    }
+//    public static void clear() {
+//        for (int i = 0; i < 100; i++)
+//            System.out.println();
+//    }
 
     public static void Error() {
         System.out.println("Input salah!");
